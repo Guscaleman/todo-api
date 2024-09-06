786f90ff1bc670df0eccdb9129e81dfef569eef8
Guscaleman <gustavocaleman@gmail.com>
2024-09-06 11:51:13 -0400

    Dockerfile ignored

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
 import { app } from "./app";
 
 const PORT: any = process.env.PORT || 3000;
-const HOST = process.env.HOST || "0.0.0.0";
 
-app.listen(PORT, HOST, () => {
-	console.log(`Server running at http://${HOST}:${PORT}/`);
+app.listen(PORT, () => {
+	console.log(`API sucessfully started at port ${PORT}`);
 });
